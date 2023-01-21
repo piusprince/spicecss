62f31e1a1561fe455f28125b0a00d5280a9d6cf6
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-01-21 20:13:00 +0000

    style: Create gradient utilities

---
+++ src/styles/gradient.css.ts
@@ -0,0 +1,46 @@
+export const angle = {
+  "angle-0": "0deg",
+  "angle-45": "45deg",
+  "angle-90": "90deg",
+  "angle-135": "135deg",
+  "angle-180": "180deg",
+  "angle-225": "225deg",
+  "angle-270": "270deg",
+  "angle-315": "315deg",
+};
+
+export const colorStop = {
+  "color-stop-0": "0%",
+  "color-stop-10": "10%",
+  "color-stop-20": "20%",
+  "color-stop-30": "30%",
+  "color-stop-40": "40%",
+  "color-stop-50": "50%",
+  "color-stop-60": "60%",
+  "color-stop-70": "70%",
+  "color-stop-80": "80%",
+  "color-stop-90": "90%",
+  "color-stop-100": "100%",
+};
+
+export const gradient = {
+  "gradient-0": "linear-gradient({angle}, {color-stop}, {color-stop})",
+  "gradient-1":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-2":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-3":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-4":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-5":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-6":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-7":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-8":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+  "gradient-9":
+    "linear-gradient({angle}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop}, {color-stop})",
+};
